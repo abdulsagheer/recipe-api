6c5f8408705c8ebf7fb1f4c916b898f8bab6ff43
abdulsagheer <73389096+abdulsagheer@users.noreply.github.com>
2021-03-16 19:18:04 +0530

    project done

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import TagViewSet,IngradientViewSet
+from .views import TagViewSet,IngredientViewSet,RecipeViewSet
 
 
 router=DefaultRouter()
 router.register('tags',TagViewSet)
-router.register('ingredients',IngradientViewSet)
+router.register('ingredients',IngredientViewSet)
+router.register('recipes', RecipeViewSet)
+
 
 app_name='recipe'
 
